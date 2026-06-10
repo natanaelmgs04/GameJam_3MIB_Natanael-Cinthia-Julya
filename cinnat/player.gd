57d73cc89d07acf72437c0a0c502c91174f35ba9
natanaelmgs04 <natanaelmgs04@gmail.com>
2026-06-10 11:24:57 -0300

    terminando o personagem

--- cinnat/player.gd
+++ cinnat/player.gd
@@ -1,39 +1,46 @@
 extends CharacterBody2D
-const SPEED = 300.0
+ 
+const WALK_SPEED = 300.0
+const RUN_SPEED = 500.0
 const JUMP_VELOCITY = -400.0
+ 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+ 
+func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	# Handle jump.
+ 
+	# Pulo
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("left", "right")
-	
-	# Inverte o sprite
+ 
+	# Movimento
+	var direction = Input.get_axis("left", "right")
+	var is_running = Input.is_action_pressed("run")
+ 
+	var speed = RUN_SPEED if is_running else WALK_SPEED
+ 
+	# Inverter sprite
 	if direction > 0:
 		animated_sprite_2d.flip_h = false
-		
 	elif direction < 0:
 		animated_sprite_2d.flip_h = true
-	# Altera a animação
+ 
+	# Animações
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite_2d.play("idle")
+		elif is_running:
+			animated_sprite_2d.play("run")
 		else:
 			animated_sprite_2d.play("walk")
 	else:
 		animated_sprite_2d.play("jump")
-		
-	# Apply movement
+ 
+	# Movimento horizontal
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed)
+ 
 	move_and_slide()
